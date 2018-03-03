290891e022daf42e25b29e772556393653c23474
Mustaqiim Muhar <mustaqiim.muhar@u.nus.edu>
2018-03-03 13:22:06 +0800

    Working API Call, formatting still broken

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net.Http;
 using Microsoft.AspNetCore.Mvc;
 using diary.Models;
 
@@ -10,6 +11,20 @@ namespace diary.Controllers
 {
     public class HomeController : Controller
     {
+
+        //public static readonly HttpClient client = new HttpClient();
+
+        //static async Task<Diary> GetDiaryAsync(string path)
+        //{
+        //    Diary diary = null;
+        //    HttpResponseMessage response = await client.GetStringAsync(path);
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        diary = await response.Content.ReadAsStreamAsync();
+        //    }
+        //    return diary;
+        //}
+
         public IActionResult Index()
         {
             return View();
@@ -25,6 +40,30 @@ namespace diary.Controllers
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Posts()
+        {
+            ViewData["Message"] = "This is diary post page.";
+
+            HttpClient client = new HttpClient();
+            //client.BaseAddress = new Uri("http://localhost:8080/api/diary");
+
+            client.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            
+            HttpResponseMessage response = client.GetAsync("http://localhost:8080/api/diary").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var dataObjects = response.Content.ReadAsStringAsync().Result;
+                foreach (var d in dataObjects)
+                {
+                    Console.WriteLine("{0}", d);
+                }
+            }
 
             return View();
         }
