0e4df0d706f150783fb60e1dd29defd697727d9f
Yee Eric <yeejfe@hotmail.com>
2018-03-04 22:07:49 +0800

    Revert "Revert "Mus apicall""

---
+++ Controllers/DiaryPostController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using diary.Data;
+using diary.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace diary.Controllers
+{
+    public class DiaryPostController : Controller
+    {
+        public ActionResult Delete(int id)
+        {
+            
+        }
+    }
+}
